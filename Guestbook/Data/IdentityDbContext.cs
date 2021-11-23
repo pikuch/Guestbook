84fd159a99c416cc586e87a649d6e845682ba353
pikuch <55052790+pikuch@users.noreply.github.com>
2021-11-23 22:18:09 +0100

    Add IdentityDbContext

---
+++ Guestbook/Data/IdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guestbook.Data
+{
+    public class IdentityDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+    : base(options)
+        {
+        }
+    }
+}
